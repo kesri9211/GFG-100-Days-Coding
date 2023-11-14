c63334afd52d91c4835cc5fbf6a9900e01db778b
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-11-14 11:15:19 +0530

    Check if strings are rotations of each other or not. -string

---
+++ Day 4/Check if strings are rotations of each other or not.java
@@ -0,0 +1,19 @@
+/*Input:
+geeksforgeeks
+forgeeksgeeks
+Output: 
+1
+Explanation: s1 is geeksforgeeks, s2 is
+forgeeksgeeks. Clearly, s2 is a rotated
+version of s1 as s2 can be obtained by
+left-rotating s1 by 5 units.*/
+
+class Solution
+{
+    //Function to check if two strings are rotations of each other or not.
+    public static boolean areRotations(String s1, String s2 )
+    {
+        // Your code here
+        return new String(s1+s1).contains(s2);
+        }
+}
