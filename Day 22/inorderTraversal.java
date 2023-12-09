8e0cfa540aebae68c8b05fd1a81fb1cb12ff5efa
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-12-09 12:37:12 +0530

    inorder Traversal - binary tree

--- Day 22/inorderTraversal.java
+++ Day 22/inorderTraversal.java
@@ -25,3 +25,20 @@ class Solution {
         return arr;
     }
 }
+
+
+//method 2
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        helper(root,res);
+        return res;
+    }
+    public void helper(TreeNode root, List<Integer> res){
+        if(root!=null){
+            helper(root.left,res);
+            res.add(root.val);
+            helper(root.right,res);
+        }
+    }
+}
