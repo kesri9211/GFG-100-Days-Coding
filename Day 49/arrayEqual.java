0024ef52af9a8c529d0a5f26b929c7afc96d7085
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-12-30 01:27:25 +0530

    Check if two arrays are equal or not

---
+++ Day 49/arrayEqual.java
@@ -0,0 +1,25 @@
+/*Given two arrays A and B of equal size N, the task is to find if given arrays are equal or not. Two arrays are said to be equal if both of them contain same set of elements, arrangements (or permutation) of elements may be different though.
+Note : If there are repetitions, then counts of repeated elements must also be same for two array to be equal.
+
+Example 1:
+Input:
+N = 5
+A[] = {1,2,5,4,0}
+B[] = {2,4,5,0,1}
+Output: 1
+Explanation: Both the array can be 
+rearranged to {0,1,2,4,5}*/
+
+class Solution{
+    //Function to check if two arrays are equal or not.
+    public static boolean check(long A[],long B[],int N)
+    {
+        //Your code here
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i=0;i<N;i++){
+            if(A[i]!=B[i]) return false;
+        }
+        return true;
+    }
+}
