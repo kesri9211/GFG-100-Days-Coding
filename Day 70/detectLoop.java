2cd967bc77a0a7da78086938e95082d1d4bd9e00
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2024-01-19 23:41:47 +0530

    detect loop in linkedlist- Linked list

---
+++ Day 70/detectLoop.java
@@ -0,0 +1,29 @@
+/*Given a linked list of N nodes. The task is to check if the linked list has a loop. Linked list can contain self loop.
+
+Example 1:
+
+Input:
+N = 3
+value[] = {1,3,4}
+x(position at which tail is connected) = 2
+Output: True
+Explanation: In above test case N = 3.
+The linked list with nodes N = 3 is
+given. Then value of x=2 is given which
+means last node is connected with xth
+node of linked list. Therefore, there
+exists a loop.*/
+class Solution {
+    //Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head){
+        // Add code
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)return true;
+        }
+        return false;
+    }
+}
